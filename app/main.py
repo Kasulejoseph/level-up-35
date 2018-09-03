bf51eb1849548d532f138d4b6a74c6f79c8ef620
kasulejoseph <kasule06joseph@gmail.com>
2018-09-03 14:23:32 +0300

    modify main.py not to accept empty input or white spaces

--- app/main.py
+++ app/main.py
@@ -3,17 +3,19 @@ from reception.reception import vip,ordinary
 def registrationCkecker(fnames):
     ordin = ordinary()
     VIP = vip()
-    for name in VIP:
-        fname = name[0]
-        lname = name[1]
-        if fnames.lower() == fname:
-            return "Name : " + fname + ' ' + lname +", Category: VIP"
+    if not fnames or fnames.isspace() :
+        return "Invalid input"
+    else:
+        for name in VIP:
+            fname = name[0]
+            lname = name[1]
+            if fnames.lower() == fname or fnames.lower() ==lname:
+                return "Name : " + fname + ' ' + lname +", Category: VIP"
 
-
-    for name in ordin:
-        fname = name[0]
-        lname = name[1]
-        if fnames.lower() == fname.lower():
-            return "Name : " + fname + ' ' + lname +", Category: Ordinary"
-    return "Not Registered"
+        for name in ordin:
+            fname = name[0]
+            lname = name[1]
+            if fnames.lower() == fname.lower() or fnames.lower() ==lname.lower():
+                return "Name : " + fname + ' ' + lname +", Category: Ordinary"
+        return "Not Registered"
 print(registrationCkecker(fnames = input("Enter name: ")))
