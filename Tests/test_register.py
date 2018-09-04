4fb423e3bebf5ffddb7752870429f8dcad82652e
kasulejoseph <kasule06joseph@gmail.com>
2018-09-04 14:58:15 +0300

    [Finishes #160250021] add tests for the UI

---
+++ Tests/test_register.py
@@ -0,0 +1,45 @@
+import unittest
+from register import signUp
+
+class testRegister(unittest.TestCase):
+
+
+    def setUp(self):
+        self.signup = signUp()
+        self.signup.firstName("name","dsjh 5fdg")
+        self.signup.lastName("name","joseph")
+        self.signup.email("name","kasule@gmail.com")
+
+    def test_creation(self):
+        self.assertIsInstance(self.signup,signUp)
+    
+    def test_firstName_exists(self):
+        self.signup.firstName("name","kasule")
+        self.assertEqual(len(self.signup.fname),1)
+
+    def test_lastName_exists(self):
+        self.signup.lastName("name","joseph")
+        self.assertEqual(len(self.signup.lname),1)
+
+    def test_email_exists(self):
+        self.signup.email("name","kasule@gmail.com")
+        self.assertEqual(len(self.signup.emails),1)
+
+    def test_is_string(self):
+        self.assertRaises(TypeError,self.signup.fname)
+        self.assertRaises(TypeError,self.signup.lname)
+        self.assertRaises(TypeError,self.signup.emails)
+
+    def test_few_characters(self):
+        '''
+        Test if inputs are enough characters
+        '''
+        self.assertTrue(self.signup.fname)
+        self.assertTrue(self.signup.lname)
+        self.assertTrue(self.signup.emails)
+
+    def test_for_valid_email(self):
+        '''
+        Test for a valid email address
+        '''
+        self.assertIn('@', self.signup.emails["name"])
